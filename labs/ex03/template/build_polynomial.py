f4ede54bbdb3e498000ed3576b35f3a1d0f1b12c
zhaoshihan <zhaosh_1997@163.com>
2020-10-07 09:15:37 +0200

    完成exercise3

--- labs/ex03/template/build_polynomial.py
+++ labs/ex03/template/build_polynomial.py
@@ -12,4 +12,21 @@ def build_poly(x, degree):
     # this function should return the matrix formed
     # by applying the polynomial basis to the input data
     # ***************************************************
-    raise NotImplementedError
+#     N = len(x)
+#     ans = np.ones(N)
+#     former = ans
+    
+#     for _ in range(degree):
+#         arr = former * x
+#         ans.vstack(arr)
+#         former = arr
+    
+#     return ans.reshape((N, degree + 1))
+    N = len(x)
+    
+    ans = np.ones((degree + 1, N))
+    for d in range(1, degree + 1):
+        arr = ans[d - 1] * x
+        ans[d] = arr
+    
+    return ans.T
